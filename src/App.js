ca1d207577de20e6b8b160d0324143f63f302e56
erikamats <erikamats@hotmail.com>
2018-06-22 17:50:11 -0500

    final

--- src/App.js
+++ src/App.js
@@ -14,20 +14,22 @@ class App extends Component {
 
   cardClick = id => {
     // if this id is clicked, add to clicked Array,
-    //   if clicked and id is not in clicked Array, (1)add to array, (2)add points, (3)randomize order
+    //   if clicked and id is not in clicked Array, (1)add to array, 
 
     if (this.state.clicked.includes(id)) {
       this.setState({
         clicked: [],
         alert: "OOPS - That's the Game!"
       });
-      this.clearAlert();
+      this.showAlert();
+
+
     } else if (this.state.clicked.length + 1 === cast.length) {
       this.setState({
         clicked: [],
         alert: "You Win!"
       });
-      this.clearAlert();
+      this.showAlert();
     } else {
       //  if clicked and matches id in array then reset score, no points
       this.setState({
@@ -37,23 +39,24 @@ class App extends Component {
     }
   };
 
-  clearAlert = () => {
+  showAlert = () => {
     setTimeout(() => {
       this.setState({
         alert: ""
       });
-    }, 2250);
+    }, 4000);
   };
 
-  // Map over this.state.cast and render a CastCard component for each cast member object
+  
   render() {
-    // randomize cards
+    // (3)randomize order - randomize cards 
     const randomCast = cast.sort(() => 0.9 - Math.random());
+    // Map over this.state.cast and render a CastCard component for each cast member object
     return (
       <Wrapper>
         <Title>
-          <div className="score">score: {this.state.clicked.length}</div>
-
+          <div className="score"><h3>score: {this.state.clicked.length} </h3></div>
+          
           {this.state.alert && <Title>{this.state.alert}</Title>}
         </Title>
 
